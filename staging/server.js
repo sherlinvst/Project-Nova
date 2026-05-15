80dca0b179f69ea3ed2fa1c298cb26ce099f1a8a
Jed Edison Panopio <jedisoniiiii@gmail.com>
2026-05-15 17:01:20 +0800

    Added server setup for payment gateway.

---
+++ staging/server.js
@@ -0,0 +1,14 @@
+// server.js
+
+const express = require("express");
+const authRoutes = require("./authService");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
